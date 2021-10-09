cc04305fc388e4b983e2fdbc3854d927d35ea142
Sam Matthews <sam.matthews.77@gmail.com>
2021-10-10 10:20:10 +1100

    added graph titles and extra info on previous months spend

--- submission file.R
+++ submission file.R
@@ -11,7 +11,7 @@ planned_spend
 ## Create A Plot of Sales, Tv And Digital Investment In The Y Axis With Time In The X Axis ####
 spend_and_sales <- data %>% select(2:4)
 color = rainbow(3)
-ts.plot(spend_and_sales,col=color)
+ts.plot(spend_and_sales,col=color, ylab='Sales/Investment', main = "Investment and sales over time")
 legend("topleft", legend = colnames(spend_and_sales), lty=1, col=color)
 abline(v=5)
 abline(v=16)
@@ -20,8 +20,8 @@ abline(v=12)
 
 ## Report On The Correlations Among Sales Tv And Digital Investments ####
 # We can see from the graph that there is some positive correlation between sales and spend as shown 
-#by many of the peaks in sales corresponding to a peak in one of the spend amounts as shown in the 5th, 
-#12th, 16th and 20th months. The level of correlation will need further investigation.
+# by many of the peaks in sales corresponding to a peak in one of the spend amounts as shown in the 5th, 
+# 12th, 16th and 20th months. The level of correlation will need further investigation.
 
 ## Fit A Reggression Model On The Data
 fit <- lm(data$sales~.,data = data)
@@ -42,21 +42,24 @@ summary(fit)
 ## Calculate The Contribution From Tv Spend To Sales In % And Absolute Dollar Value ####
 (1.947/(-5.287+1.947+2.872+1.419e+07+3.097e+06))*100
 # Therefore tv_spend contributes 0.000112628% to the total contribution towards sales
-# In terms of absolute dollar amount, it contributes $1.947 for every extra dollar spent
+# In terms of absolute dollar amount, it contributes $1.947 for every extra dollar spent.
 
 ## Calculate Tv ROI ####
 total_sales =sum(data$sales)
 total_spend = sum(data$tv_spend)
 ROI = (total_sales/total_spend)*100
 ROI
+# Therefore the total return on investment for the 2 year period is $1043.052 for every dollar of tv_spend.
 
-## Calculate The Expected Value For The First 3 Months Of 2018
+## Calculate The Expected Value For The First 3 Months Of 2018 ####
 planned_spend$trend <- c(25,26,27)
 planned_spend$xmas <- c(0,0,0)
 planned_spend
 predict.lm(fit, newdata = planned_spend)
+# Therefore, for the first three months of 2018 the model predicts $11,750,228 for January, $12,760,264
+# for February and $15,656,009 for March.
 
-## What Additional Data Would Improve Your Model
+## What Additional Data Would Improve Your Model ####
 # There are a number of additional variables that could improve the model. In terms of the tv_spend, it 
 # would be useful to know what level of the products target audience (which could be found through 
 # unsupervised clustering) was reached along with the frequency according to tv ratings. This could be 
@@ -67,6 +70,10 @@ predict.lm(fit, newdata = planned_spend)
 # on the ad as well as tracking through what percentage of those clicks led to sales would allow us to
 # better understand the impact of the tv vs digital advertisements. A breakdown of spending on each 
 # social media or ad services would also be useful to see the impact of spending on different services.
+#
+# Some investment for both TV and digital may also have a delayed payout. If an ad is launched laye in 
+# October we may not see sales until November. Because of this, it could be useful to include extra 
+# variables indicating the previous months advertising spend.
 #
 # It would also be useful to know when new toy lines are being released as this could result in a
 # boosted sales level for the release month and well the subsequent months having variables similar to 
